0cef8af51572dd2a7ebf3aabc6c56706c79f4cba
Yash Pandya <yash.pandya@briskteq.com>
2023-03-22 17:03:41 +0530

    contact page created

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,45 @@
-import { Link } from "gatsby";
 import React from "react";
 import Layout from "../components/Layout";
 
 const contact = () => {
   return (
     <Layout>
-      <h1>contact</h1>
-      <Link to="/">Back</Link>
+      <main className="page">
+        <section className="contact-page">
+          <article className="contact-info">
+            <h3>Want to Get in Touch ?</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+              nihil ab eveniet omnis dignissimos veniam quos, doloribus tempora
+              fuga eius.
+            </p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum
+              odit, dolor excepturi quo exercitationem ad!
+            </p>
+          </article>
+          <article>
+            <form action="" className="form contact-form">
+              <div className="form-row">
+                <label htmlFor="name">Your Name</label>
+                <input type="text" name="name" id="name" />
+              </div>
+              <div className="form-row">
+                <label htmlFor="email">Your Email</label>
+                <input type="email" name="email" id="email" />
+              </div>
+              <div className="form-row">
+                <label htmlFor="message">Message</label>
+                <textarea name="message" id="message"></textarea>
+              </div>
+              <button type="submit" className="btn block">
+                Connect
+              </button>
+            </form>
+          </article>
+        </section>
+      </main>
     </Layout>
   );
 };
