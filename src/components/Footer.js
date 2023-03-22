cdbaa7a733ea4c9011143fb591bc1fa0917a68a5
Yash Pandya <yash.pandya@briskteq.com>
2023-03-22 16:32:21 +0530

    footer css added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="page-footer">
+      <p>
+        &copy; {new Date().getFullYear()} <span>SimplyReciepe</span>. Built with
+        ❤️
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
