2ff084544ad253aeb209f942c4d2ed571b1fd55e
Yash Pandya <yash.pandya@briskteq.com>
2023-03-24 13:11:54 +0530

    gallery rendered

--- src/pages/testing.js
+++ src/pages/testing.js
@@ -1,31 +1,14 @@
 import React from "react";
-import { graphql } from "gatsby";
-
-const Testing = ({ data }) => {
-  const author = data.site.info.author;
-
-  return <div>author: {author}</div>;
+import Layout from "../components/Layout";
+import Gallery from "../example/gallery";
+const testing = () => {
+  return (
+    <Layout>
+      <main className="page">
+        <Gallery />
+      </main>
+    </Layout>
+  );
 };
 
-export const data = graphql`
-  {
-    site {
-      buildTime
-      info: siteMetadata {
-        author
-        description
-        simpleData
-        title
-        person {
-          age
-          name
-        }
-        complexData {
-          age
-          name
-        }
-      }
-    }
-  }
-`;
-export default Testing;
+export default testing;
