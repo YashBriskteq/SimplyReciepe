7cc889554df7f87ea45540cc1046232abd116b7a
Yash Pandya <yash.pandya@briskteq.com>
2023-03-28 10:30:09 +0530

    data added

--- src/pages/{ContentfulReciepe.title}.js
+++ src/pages/{ContentfulReciepe.title}.js
@@ -3,6 +3,7 @@ import { graphql, Link } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import { BsClockHistory, BsClock, BsPeople } from "react-icons/bs";
 import Layout from "../components/Layout";
+import slugify from "slugify";
 
 const ReciepeTemplate = ({ data }) => {
   const {
@@ -27,20 +28,74 @@ const ReciepeTemplate = ({ data }) => {
             <article className="reciepe-info">
               <h2>{title}</h2>
               <p>{description}</p>
-              <div className="reciepe-icons"></div>
-              <div className="reciepe-tags">
-                Tags:
-                {tags.map((tag, index) => {
+              <div className="reciepe-icons">
+                <article>
+                  <BsClock />
+                  <h5>Prep Time</h5>
+                  <p>{prepTime} Mins.</p>
+                </article>
+                <article>
+                  <BsClockHistory />
+                  <h5>Cook Time</h5>
+                  <p>{cookTime} Mins.</p>
+                </article>
+                <article>
+                  <BsPeople />
+                  <h5>Servings</h5>
+                  <p>{servings}</p>
+                </article>
+              </div>
+              {/* <p className="reciepe-tags">
+                Tags :
+                {tags?.map((tag, index) => {
                   return (
                     <Link to={`/${tag}`} key={index}>
                       {tag}
                     </Link>
                   );
-                })}{" "}
-              </div>
+                })}
+              </p> */}
             </article>
           </section>
-          <section className="reciepe-content"></section>
+          <section className="reciepe-content">
+            <article>
+              <h4>Instructions</h4>
+              {instructions.map((item, index) => {
+                return (
+                  <div key={index} className="single-instruction">
+                    <header>
+                      <p>Step {index + 1}</p>
+                      <div></div>
+                    </header>
+                    <p>{item}</p>
+                  </div>
+                );
+              })}
+            </article>
+            <article className="second-column">
+              <div>
+                <h4>ingredients</h4>
+                {ingredients.map((item, index) => {
+                  return (
+                    <p key={index} className="single-ingredient">
+                      {item}
+                    </p>
+                  );
+                })}
+              </div>
+              {/* <div>
+                <h4>Tools</h4>
+                {tools &&
+                  tools.map((item, index) => {
+                    return (
+                      <p key={index} className="single-tool">
+                        {item}
+                      </p>
+                    );
+                  })}
+              </div> */}
+            </article>
+          </section>
         </div>
       </div>
     </Layout>
---
+++ src/template/tag-template.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TagTemplate = () => {
+  return (
+    <div>
+      <h2>tag-template</h2>
+    </div>
+  );
+};
+
+export default TagTemplate;
